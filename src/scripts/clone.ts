a27a4970ebece6bc042bdf625aa7cfd37ea59486
Andreas Coroiu <andreas.coroiu@gmail.com>
2024-09-27 14:39:23 +0200

    feat: split clone code into separate command

---
+++ src/scripts/clone.ts
@@ -0,0 +1,87 @@
+import chalk from "chalk";
+import inquirer from "inquirer";
+import { folderExists } from "../commands/folder-exists";
+import { environment } from "../environment";
+import { deleteFolder } from "../commands/delete-folder";
+import { runCommand } from "../commands/run-command";
+
+/**
+ * Clones the clients and SDK repositories
+ */
+export default async function run() {
+  console.log(chalk.green("> Running script: clone"));
+
+  const cloneClients = await checkRepositoryShouldClone(
+    "clients",
+    environment.repositories.clients.localPath
+  );
+  const cloneSdk = await checkRepositoryShouldClone("sdk", environment.repositories.sdk.localPath);
+
+  // Clone repositories
+  if (cloneClients) {
+    await runCommand(
+      `git clone --progress ${environment.repositories.clients.gitUri} ${environment.repositories.clients.localPath}`
+    );
+  }
+
+  if (cloneSdk) {
+    await runCommand(
+      `git clone --progress ${environment.repositories.sdk.gitUri} ${environment.repositories.sdk.localPath}`
+    );
+  }
+}
+
+/**
+ * Check if the repository exists and asks the user what they want to do
+ * @param name Name of the repository
+ * @param path Path to the repository
+ * @returns true if the repository should be cloned, false otherwise
+ */
+async function checkRepositoryShouldClone(name: string, path: string): Promise<boolean> {
+  if (await folderExists(path)) {
+    console.log(chalk.yellow(`Warning: Found existing ${name} repository!`));
+    const answers = await inquirer.prompt([
+      {
+        type: "select",
+        name: "delete",
+        choices: [
+          { name: "Use as-is", value: false },
+          { name: "Delete", value: true },
+        ],
+        default: false,
+        message: chalk.red(
+          `Initializing a new ${name} repository will delete the existing one. Are you sure you want to proceed?`
+        ),
+      },
+    ]);
+
+    if (!answers.delete) {
+      console.log(chalk.yellow(`Using existing ${name} repository as-is.`));
+      return false;
+    }
+
+    const confirmationAnswer = await inquirer.prompt([
+      {
+        type: "confirm",
+        name: "delete",
+        default: false,
+        message: chalk.red(
+          `Are you sure you want to proceed? This will DELETE the existing ${name} repository.`
+        ),
+      },
+    ]);
+
+    if (!confirmationAnswer.delete) {
+      console.log(chalk.yellow(`Using existing ${name} repository as-is.`));
+      return false;
+    }
+
+    console.log(chalk.yellow(`Deleting ${name} repository...`));
+    await deleteFolder(path);
+  }
+
+  return true;
+}
+
+// Run the script if it's being executed directly
+if (require.main === module) run().catch(console.error);
